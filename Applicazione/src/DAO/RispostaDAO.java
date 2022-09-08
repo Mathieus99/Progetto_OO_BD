1a2d354414a24207a1ebcaca27cb0cde81875b3f
Matteo Marino <95318540+Mathieus99@users.noreply.github.com>
2022-09-08 12:37:50 +0200

    Aggiornamento pt.2

---
+++ Applicazione/src/DAO/RispostaDAO.java
@@ -0,0 +1,110 @@
+package DAO;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+import Database.ConnessioneDatabase;
+import Model.Domanda;
+import Model.IstanzaDiTest;
+import Model.Risposta;
+import Model.RispostaUtente;
+
+public class RispostaDAO {
+	
+	Connection conn;
+	//Crea la connessione
+	
+	public RispostaDAO(){
+		try {
+			conn = ConnessioneDatabase.getInstance().getConnection();
+		} catch (SQLException e) {
+			System.out.println("Connessione fallita.");
+			e.printStackTrace();
+		}
+	}
+	
+	//--------------------
+	
+	//Cerca l'ultimo id di risposta presente nel db e lo restituisce
+	
+	public int getMaxId() {
+		int id = 0;
+		try {
+			PreparedStatement getid = conn.prepareStatement("SELECT MAX(idrisposta) FROM risposta");
+			ResultSet rs = getid.executeQuery();
+			while (rs.next())
+				id = rs.getInt("max");
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return id;
+	}
+	
+	//--------------------------------------------------------------- 
+	
+	//Cerca le possibili risposte alla relativa domanda-------------
+	public ArrayList<Risposta> getRisposte(Domanda d){
+		ArrayList<Risposta> risposteD = new ArrayList<Risposta>();
+		Risposta r;
+		try {
+			PreparedStatement getR = conn.prepareStatement("SELECT * FROM risposta WHERE iddomanda = "+d.getIdDomanda());
+			ResultSet rs = getR.executeQuery();
+			while (rs.next()) {
+				r = new Risposta();
+				r.setIdDomanda(rs.getLong("iddomanda"));
+				r.setIdRisposta(rs.getLong("idrisposta"));
+				r.setTestoRisposta(rs.getString("testorisposta"));
+				r.setCorretta(rs.getBoolean("corretta"));
+				risposteD.add(r);
+			}
+		}catch (SQLException e) {
+			System.out.println("Errore caricamento risposte!");
+			e.printStackTrace();
+		}
+		System.out.println("Risposte prelevate :"+risposteD.size());
+		return risposteD;
+	}
+	//--------------------------------------------------------------
+	
+	public void checkPunteggioRisposta(IstanzaDiTest IdT) {
+		int numeroRisposte = 0;
+		try {
+			for (RispostaUtente rU: IdT.getRisposteUtente()) {
+				PreparedStatement checkP = conn.prepareStatement("SELECT * FROM test T JOIN (domanda D JOIN risposta R ON D.iddomanda=R.iddomanda) F ON T.idtest=F.idtest WHERE testorisposta = '"+rU.getTestoRisposta()+"'");
+				ResultSet rs = checkP.executeQuery();
+				while (rs.next()) {
+					if(rs.getBoolean("corretta")) {
+						rU.setPunteggio(rs.getInt("punteggiodmax"));
+						IdT.aggiungiCorretta(rs.getInt("punteggiodmax"));
+						numeroRisposte++;
+					}
+					else {
+						rU.setPunteggio(rs.getInt("punteggiodmin"));
+						IdT.aggiungiErrata(rs.getInt("punteggiodmin"));
+						numeroRisposte++;
+					}
+				}
+				if(numeroRisposte==IdT.getTest().getNumeroDomande()) {
+					IdT.setStato(IstanzaDiTest.Valutato);
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}		
+	}
+	
+	//Salva le nuove risposte nel db
+	
+	public void saveRisposte(ArrayList<Risposta> risposte) {
+		try {
+			for (Risposta r: risposte) {
+				PreparedStatement saveR = conn.prepareStatement("INSERT INTO risposta values("+r.getIdRisposta()+","+r.getIdDomanda()+",'"+r.getTestoRisposta()+"',"+r.isCorretta()+")");
+				saveR.execute();
+			}
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//---------------------------------
+}
---
+++ Applicazione/src/DAO/StudenteDAO.java
@@ -0,0 +1,118 @@
+package DAO;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+import Database.ConnessioneDatabase;
+import Model.IstanzaDiTest;
+import Model.Studente;
+
+public class StudenteDAO {
+	Connection conn;
+	Studente s;
+	
+	public StudenteDAO(){
+		try {
+			conn = ConnessioneDatabase.getInstance().getConnection();
+		} catch (SQLException e) {
+			System.out.println("Connessione fallita.");
+			e.printStackTrace();
+		}
+	}
+	
+	public int getMaxId() {
+		int maxId = 0;
+		try {
+			PreparedStatement getId = conn.prepareStatement("SELECT MAX(idutente) FROM UTENTE");
+			ResultSet rs = getId.executeQuery();
+			while (rs.next())
+				maxId = rs.getInt("max");
+		} catch (SQLException e) {
+			System.out.println("StudenteDAO - Errore recupero max ID");
+			e.printStackTrace();
+		}
+		return maxId;
+	}
+	
+	public ArrayList<IstanzaDiTest> loadTestSostenuti (Studente s){
+		IstanzaDiTest IdT;
+		TestDAO tdao = new TestDAO();
+		ArrayList<IstanzaDiTest> testStudente = new ArrayList<IstanzaDiTest>();
+		try {
+		PreparedStatement loadTestS = conn.prepareStatement("SELECT * FROM istanzaditest WHERE studente="+s.getIdStudente());
+		ResultSet rs = loadTestS.executeQuery();		
+		if (rs.wasNull())
+			s.setTestSostenuti(null);
+		else
+			while(rs.next()) {
+				IdT = new IstanzaDiTest();
+				IdT.setIdIstanza(rs.getInt("idistanzaditest"));
+				IdT.setStato(rs.getString("stato"));
+				IdT.setPunteggio(rs.getInt("risultato"));
+				IdT.setOrarioFine(rs.getTimestamp("datasostenuto"));
+				IdT.setNumCorrette(rs.getInt("numerorcorrette"));
+				IdT.setNumErrate(rs.getInt("numerorerrate"));					
+				IdT.setTest(tdao.getTestbyID(rs.getInt("idtest")));
+				testStudente.add(IdT);
+			}
+		s.setTestSostenuti(testStudente);
+		} catch (SQLException e) {
+			System.out.println("Errore nel caricamento dei test dello studente");
+			e.printStackTrace();
+		}
+		return testStudente;
+	}
+	
+	public void register(Studente s) {
+		try {
+			PreparedStatement registra = conn.prepareStatement("INSERT INTO utente(idutente,nome,cognome,email,password) VALUES("+s.getIdStudente()+",'"+s.getNome()+"','"+s.getCognome()+"','"+s.getEmail()+"','"+s.getPassword()+"')");
+			registra.execute();
+		} catch (SQLException e) {
+			System.out.println("StudenteDAO - Errore Registrazione Studente");
+			e.printStackTrace();
+		}
+	}
+	
+	public Studente login(String email, String pass){
+		s= new Studente();
+		ResultSet rs;
+		try {
+			PreparedStatement login = conn.prepareStatement("SELECT * FROM utente WHERE email = '" + email + "' AND password = '" + pass + "' AND ruolo = 'Studente'");
+			rs = login.executeQuery();
+			if (rs.wasNull())
+				return null;	
+			while(rs.next()) {
+				s.setIdStudente(rs.getLong("idutente"));
+				s.setNome(rs.getString("nome"));
+				s.setCognome(rs.getString("cognome"));
+				s.setEmail("email");
+				s.setPassword("password");
+			}
+			rs.close();
+			s.setTestSostenuti(loadTestSostenuti(s));
+			return s;
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+			return null;
+		}			
+	}
+	
+	public Studente setStudenteIstanza(int id) {
+		Studente s = new Studente();
+		try {
+			PreparedStatement search = conn.prepareStatement("SELECT * FROM utente where idutente ="+ id);
+			ResultSet rs = search.executeQuery();
+			while (rs.next()) {
+				s.setIdStudente(rs.getLong("idutente"));
+				s.setNome(rs.getString("nome"));
+				s.setCognome(rs.getString("cognome"));
+				s.setEmail(rs.getString("email"));
+			}
+		} catch (SQLException e) {
+			System.out.println("Set Studente Istanza - Errore nel cercare lo studente");
+			e.printStackTrace();
+		}
+		return s;
+	}
+}
